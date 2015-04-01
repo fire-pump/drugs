53635cbbf53f14bf928d3388fda1cfd14bf9067e
robert <robert@roberts-Mac.local>
2015-04-01 17:31:53 +0300

    Initial Commit

---
+++ drugs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  drugs
+//
+//  Created by robert on 4/1/15.
+//  Copyright (c) 2015 robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
