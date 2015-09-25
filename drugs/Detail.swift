2757238f1674ef4b177e7401a345d137ef335f78
fire-pump <Angelofmars18@gmail.com>
2015-09-26 02:33:55 +0300

    lucas
    json+

--- drugs/Detail.swift
+++ drugs/Detail.swift
@@ -21,10 +21,62 @@ class Detail: UIViewController {
     var id: Int
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        Did.text = "\(id)"
-    }
+        let endpoint = NSURL(string: "http://jesuscodes.me/drugs/list.json")
+        var data = NSData(contentsOfURL: endpoint!)
+        if let json: NSDictionary = NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers, error: nil) as? NSDictionary {
+            if let items = json["items"] as? NSArray {
+                for item in items {
+                    class Repo {
+                        
+                        var idi : Int?
+                        var name: String?
+                        var description: String?
+                        var usage: String?
+                        var affect: String?
+                        var cautions : String?
+                        var addiction: Int?
+                        var price : Int?
+                        var cover : String?
+                        
+                        init(json: NSDictionary) {
+                            if let i = json["id"] as? Int {
+                                self.idi = i
+                            }
+                            if let n = json["name"] as? String {
+                                self.name = n
+                            }
+                            if let d = json["description"] as? String {
+                                self.description = d
+                            }
+                            if let u = json["usage"] as? String {
+                                self.usage = u
+                            }
+                            if let a = json["affect"] as? String {
+                                self.affect = a
+                            }
+                            if let c = json["cautions"] as? String {
+                                self.cautions = c
+                            }
+                            if let ad = json["addiction"] as? Int {
+                                self.addiction = ad
+                            }
+                            if let p = json["price"] as? Int {
+                                self.price = p
+                            }
+                            if let url = json["cover"] as? String {
+                                self.cover = url
+                            }
+                        }
+                    }
+                   
+                        
+                    }
+                }
+            }        // Dispose of any resources that can be recreated.
+        }
     
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
